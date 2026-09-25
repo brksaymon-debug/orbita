ac92a87ab0941ba3c6ff5b9fab990ca3ce296f0d
brksaymon-debug <brksaymon@gmail.com>
2026-09-25 20:15:18 +0800

    Mobile feed: Nike spot (work-18) first; seamless crossfade loop for hero bg video

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='orbita-app-v10-form';
+const CACHE='orbita-app-v11-nike';
 self.addEventListener('install', e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', e=>{ e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', e=>{
