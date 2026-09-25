89b342c7b44522255a185ad035fd1ebb2a3d3ee5
brksaymon-debug <brksaymon@gmail.com>
2026-09-25 17:39:16 +0800

    Pac-Man: run on reduced-motion and width-based desktop; SW v8; bust 202609251745

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='orbita-app-v7-perf';
+const CACHE='orbita-app-v8-pacman';
 self.addEventListener('install', e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', e=>{ e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', e=>{
